1eea483074ea942576019672eff6829a9a7537ec
user <johnnydiabetic@gmail.com>
2014-05-19 22:14:59 -0400

    Added command line arguments and made the output usable

---
+++ README.md
@@ -0,0 +1,6 @@
+Import Checker
+================
+
+Small utility to check imports for your python project
+
+A work in progress - need to parse out the actual libraries
--- dirwalker.py
+++ dirwalker.py
@@ -7,7 +7,8 @@ def get_import_dict(path=None, walk=False):
         
     files = [fn for fn in os.listdir(path) if os.path.isfile(os.path.join(path, fn))]
     dirs = [os.path.join(path, d) for d in os.listdir(path) if os.path.isdir(os.path.join(path,d))]
-    #print path + ": " + str(dirs)
+    
+    
     i_dict = {}
     for f in files:
         with open(os.path.join(path,f)) as fname:
@@ -19,28 +20,56 @@ def get_import_dict(path=None, walk=False):
                     except (KeyError):
                         i_dict[os.path.join(path,f)] = []
                         i_dict[os.path.join(path,f)].append(l)
+    
     if walk:
         if dirs == []:
-            return (i_dict, None)
+            p = []
+            p.append(path)
+            return (i_dict, p)
         else:
             new_dirs = []
             for d in dirs:
                 p = os.path.join(path,d)
                 (x,y) = get_import_dict(p, True)
                 i_dict.update(x)
-                new_dirs.append(y)
+                new_dirs+=y
+                
             dirs+=new_dirs
-    
+     
+    dirs = list(set(dirs))       
     return (i_dict, dirs)
 
-def main():
-    (x,y) = get_import_dict(walk= True)
-    for k,v in x.iteritems():
-        print k + " : " + str(v)
-        
-    #for i in y:
-    #    print i
+def output_results(import_tuple):
+    for k,v in import_tuple[0].iteritems():
+            print k + " : "
+            for i in v:
+                print "\t"+str(i).rstrip()
+            print   
     
+    if len(import_tuple[0]):    
+        print str(len(import_tuple[0].keys())) + " files found | " + str(len(import_tuple[1])) + " directories scanned"
+    else:
+        print " 0 files found | " + str(len(import_tuple[1])) + " directories scanned"
+        
+            
+    
+
+def main():
+    path = None
+    if len(sys.argv)==2:
+        path = sys.argv[1]
+        (x,y) = get_import_dict(path, walk= True)
+        output_results((x,y))
+        
+    elif len(sys.argv) > 2:
+        print "too many command line arguments"
+    
+    else:
+        path = None
+        (x,y) = get_import_dict(path, walk= True)
+        output_results((x,y))
+            
+        
 if __name__=='__main__':
     status = main()
     sys.exit(status)
